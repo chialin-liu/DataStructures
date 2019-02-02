f4fedee53a85d3973b8bb362d5a4d8cf1eea7c39
chialin-liu <charles.ee96@g2.nctu.edu.tw>
2019-02-02 17:12:17 +0800

    Signed-off-by: chialin-liu <charles.ee96@g2.nctu.edu.tw>

--- Tree/BinarySearchTree/ConstructAllPossibleBSTs/src/ConstructAllPossibleBSTs.java
+++ Tree/BinarySearchTree/ConstructAllPossibleBSTs/src/ConstructAllPossibleBSTs.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
+class TreeNode{
+        int val;
+        TreeNode left,right;
+        TreeNode(int a){
+            val=a;
+            left=right=null;
+        }
+}
 public class ConstructAllPossibleBSTs {
 }
