dc8b15bf44a972406e9546c57d4eda800782e8aa
chialin-liu <charles.ee96@g2.nctu.edu.tw>
2019-02-02 18:22:33 +0800

    Not finished

--- Tree/BinarySearchTree/ConstructAllPossibleBSTs/src/ConstructAllPossibleBSTs.java
+++ Tree/BinarySearchTree/ConstructAllPossibleBSTs/src/ConstructAllPossibleBSTs.java
@@ -1,3 +1,8 @@
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class TreeNode{
         int val;
         TreeNode left,right;
@@ -7,4 +12,13 @@ class TreeNode{
         }
 }
 public class ConstructAllPossibleBSTs {
+        public List<TreeNode> construct_BST(int n){
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            TreeNode root = null;
+            for(int i =0;i<=n;i++){
+                root = TreeNode(i);
+
+            }
+        }
+
 }
