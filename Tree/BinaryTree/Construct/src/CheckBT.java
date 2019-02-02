719ceaf78da00383b6794fa528ff36b4cfcd6724
chialin-liu <charles.ee96@g2.nctu.edu.tw>
2019-02-02 18:26:29 +0800

    check whether two binary tree are the same

---
+++ Tree/BinaryTree/Construct/src/CheckBT.java
@@ -0,0 +1,13 @@
+class TreeNode{
+    int val;
+    TreeNode left,right;
+    TreeNode(int a){
+        val=a;
+        left=right=null;
+    }
+}
+public class CheckBT {
+    public boolean checkSameBT(TreeNode A, TreeNode B){
+
+    }
+}
