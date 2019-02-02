9e79aed2dffe42f86303dba89bdfcf546f852050
chialin-liu <charles.ee96@g2.nctu.edu.tw>
2019-02-02 16:22:19 +0800

    Inoder_stack

--- Tree/Traverse/Inorder/src/Inorder.java
+++ Tree/Traverse/Inorder/src/Inorder.java
@@ -1,4 +1,7 @@
 //package com.company;
+
+import java.util.Stack;
+
 class TreeNode{
     int val;
     TreeNode left,right;
@@ -17,7 +20,25 @@ class Inoder {
         System.out.println(root.val);
         inorder_recursive(root.right);
     }
-
+    public void inorder_stack(TreeNode root){
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        TreeNode pop = null;
+        if(cur==null){return ;}
+        stack.push(cur);
+        while(!stack.isEmpty()) {
+            while (cur.left != null) {
+                cur = cur.left;
+                stack.push(cur);
+            }
+            pop = stack.pop();
+            System.out.println(pop.val);
+            cur = pop.right;
+            if(cur!=null){
+                stack.push(cur);
+            }
+        }
+    }
     public static void main(String[] args) {
         // write your code here
         TreeNode root = new TreeNode(1);
@@ -26,14 +47,16 @@ class Inoder {
         TreeNode C = new TreeNode(7);
         TreeNode D = new TreeNode(9);
         TreeNode E = new TreeNode(3);
+        TreeNode F = new TreeNode(10);
 
         root.left = A;
         root.right = B;
         A.left = C;
         A.right = D;
         B.left = E;
+        D.left = F;
         Inoder s1 = new Inoder();
-        s1.inorder_recursive(root);
+        s1.inorder_stack(root);
     }
 }
 
