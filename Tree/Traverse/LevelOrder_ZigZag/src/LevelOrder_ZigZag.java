25b187bb79c6e08bbd3ac1d2ff13c169e8a397ba
chialin-liu <charles.ee96@g2.nctu.edu.tw>
2019-02-03 12:13:57 +0800

    ZZ scan for LevelOrder

--- Tree/Traverse/LevelOrder_ZigZag/src/LevelOrder_ZigZag.java
+++ Tree/Traverse/LevelOrder_ZigZag/src/LevelOrder_ZigZag.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 class TreeNode{
     int val;
     TreeNode left,right;
@@ -7,4 +11,8 @@ class TreeNode{
     }
 }
 public class LevelOrder_ZigZag {
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    Stack<TreeNode> stack_right=new Stack<TreeNode>();
+    Stack<TreeNode> stack_left=new Stack<TreeNode>();
 }
+
